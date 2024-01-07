db787c7e4b8f27df48513376a95625ca531c8bdc
yokobond <koji.yokokawa@gmail.com>
2024-01-07 16:48:57 +0900

    Fix test

--- test/unit/block.test.js
+++ test/unit/block.test.js
@@ -6,16 +6,12 @@ describe("blockClass", () => {
     const runtime = {
         formatMessage: function (msg) {
             return msg.default;
-        }
+        },
+        on: function () { },
     };
     it("should create an instance of blockClass", () => {
         const block = new blockClass(runtime);
         expect(block).to.be.an.instanceOf(blockClass);
     });
-
-    it("doIt('3 + 4') should return 7", () => {
-        const block = new blockClass(runtime);
-        const result = block.doIt({SCRIPT: "3 + 4"});
-        expect(result).to.equal(7);
-    });
 });
+
